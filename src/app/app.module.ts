5f2c22ffd2ad3ddc3642822d6bc1a8bbebca2dbd
dancer1325 <alfredotic0809@gmail.com>
2022-01-13 14:55:36 +0100

    Commentaries about router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
 // Rutas
-import { APP_ROUTING } from './app.routes';
+import { APP_ROUTING } from "./app.routes";
 
 // Servicios
-import { HeroesService } from './servicios/heroes.service';
+import { HeroesService } from "./servicios/heroes.service";
 
 // Componentes
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
-import { HeroeComponent } from './components/heroe/heroe.component';
-import { BuscadorComponent } from './components/buscador/buscador.component';
-import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+import { HeroesComponent } from "./components/heroes/heroes.component";
+import { HeroeComponent } from "./components/heroe/heroe.component";
+import { BuscadorComponent } from "./components/buscador/buscador.component";
+import { HeroeTarjetaComponent } from "./components/heroe-tarjeta/heroe-tarjeta.component";
 
 @NgModule({
   declarations: [
@@ -28,17 +28,11 @@ import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.
     HeroesComponent,
     HeroeComponent,
     BuscadorComponent,
-    HeroeTarjetaComponent
+    HeroeTarjetaComponent,
+    // APP_ROUTING // We get an error in code time, that something is wrong
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    APP_ROUTING
-  ],
-  providers: [
-    HeroesService
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule, HttpModule, APP_ROUTING],
+  providers: [HeroesService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
